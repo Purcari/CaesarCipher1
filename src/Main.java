d77af13d840f55c1fb1453be4941790a4526cb70
Sabina B <sabina.motrescu@gmail.com>
2023-01-17 13:42:37 +0200

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -30,12 +30,4 @@ public class Main {
 
 
 
-    public static void main(String[] args) {
-        String message = "we are testing";
-        System.out.println("we are testing");
-
-    }
-
-
-
-}
+    
