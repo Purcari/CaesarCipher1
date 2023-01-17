22e7f3955b9d665c242b8ffaa83717b56557de78
Purcari <sabina.motrescu@gmail.com>
2023-01-17 13:41:34 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,41 @@
+import java.lang.constant.Constable;
+
+public class Main {
+
+    public static String encrypt(String weareTesting, int shift) {
+        StringBuilder encryptedText = new StringBuilder();
+        int shifta = 3;
+        for (int i = 0; i < weareTesting.length(); i++) {
+            char c = weareTesting.charAt(i);
+            if (Character.isLetter(c)) {
+                if (Character.isLowerCase(c)) {
+                    char newC = (char) ((c - 'a' + shift) % 26 + 'a');
+                    encryptedText.append(newC);
+                } else {
+                    char newC = (char) ((c - 'A' + shift) % 26 + 'A');
+                    encryptedText.append(newC);
+                }
+            } else {
+                encryptedText.append(c);
+            }
+        }
+        return encryptedText.toString();
+    }
+
+
+
+
+
+
+
+
+
+    public static void main(String[] args) {
+        String message = "we are testing";
+        System.out.println("we are testing");
+
+    }
+
+
+
+}
